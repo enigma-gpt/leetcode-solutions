77f80df6af366d9ad9d6bbcb5808f00279d98877
Grzegorz Talaga <Grzegorz.Talaga@hltech.com>
2022-08-26 09:39:16 +0200

    split array into consecutive subsequences 659

--- src/main/python/com/enigmagpt/learning/leetcode/algo/split-array-into-consecutive-subsequences-659.py
+++ src/main/python/com/enigmagpt/learning/leetcode/algo/split-array-into-consecutive-subsequences-659.py
@@ -20,3 +20,22 @@ class Solution:
         return True
 
 
+# Example 1:
+#
+# Input: nums = [1,2,3,3,4,5]
+# Output: true
+# Explanation: nums can be split into the following subsequences:
+# [1,2,3,3,4,5] --> 1, 2, 3
+# [1,2,3,3,4,5] --> 3, 4, 5
+# Example 2:
+#
+# Input: nums = [1,2,3,3,4,4,5,5]
+# Output: true
+# Explanation: nums can be split into the following subsequences:
+# [1,2,3,3,4,4,5,5] --> 1, 2, 3, 4, 5
+# [1,2,3,3,4,4,5,5] --> 3, 4, 5
+# Example 3:
+#
+# Input: nums = [1,2,3,4,4,5]
+# Output: false
+# Explanation: It is impossible to split nums into consecutive increasing subsequences of length 3 or more.
