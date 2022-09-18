873173f55373618d0d0425cae3a9b1f4d04cfc4f
Grzegorz <grzechtl@gmail.com>
2022-09-18 12:17:37 +0200

    Remove element 27

---
+++ src/main/python/com/enigmagpt/learning/leetcode/algo/remove_element_27.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+
+        i = 0
+
+        for num in nums:
+            if num != val:
+                nums[i] = num
+                i += 1
+
+        return i
+
+
+llist = [3, 2, 2, 3]
+x = Solution.removeElement(Solution, llist, 3)
+print(llist)
+print(x)
