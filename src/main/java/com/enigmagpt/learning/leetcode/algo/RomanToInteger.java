8ce71d3b6517564f81f03bccd67cc16ff35f6b73
Grzegorz <grzechtl@gmail.com>
2022-10-31 22:24:18 +0100

    Roman to Integer v2

--- src/main/java/com/enigmagpt/learning/leetcode/algo/RomanToInteger.java
+++ src/main/java/com/enigmagpt/learning/leetcode/algo/RomanToInteger.java
@@ -1,53 +1,58 @@
 package com.enigmagpt.learning.leetcode.algo;
 
-import java.util.Map;
-import java.util.Stack;
-
 public class RomanToInteger {
 
     static class Solution {
         public int romanToInt(String s) {
 
-            final Map<Character, Integer> charMap = Map.of(
-                    'I', 1,
-                    'V', 5,
-                    'X', 10,
-                    'L', 50,
-                    'C', 100,
-                    'D', 500,
-                    'M', 1000
-            );
-
             int sum = 0;
-
-            boolean skipNext = false;
             char[] charArray = s.toCharArray();
 
-            for (int i = 0; i < charArray.length; i++) {
+            int i = 0;
+            while (i < charArray.length) {
 
-                if (skipNext) {
-                    skipNext = false;
-                    continue;
-                }
-
-                int currentNumber = charMap.get(charArray[i]);
+                int currentNumber = toInt(charArray[i]);
 
                 if (i + 1 < charArray.length) {
-                    int nextNumber = charMap.get(charArray[i + 1]);
+                    int nextNumber = toInt(charArray[i + 1]);
 
                     if (currentNumber >= nextNumber) {
                         sum += currentNumber;
                     } else {
                         sum += nextNumber - currentNumber;
-                        skipNext = true;
+                        i++;
                     }
 
                 } else {
                     sum += currentNumber;
                 }
+
+                i++;
             }
 
             return sum;
+        }
+
+        private static int toInt(char c) {
+
+            switch (c) {
+                case 'I':
+                    return 1;
+                case 'V':
+                    return 5;
+                case 'X':
+                    return 10;
+                case 'L':
+                    return 50;
+                case 'C':
+                    return 100;
+                case 'D':
+                    return 500;
+                case 'M':
+                    return 1000;
+            }
+
+            return -1;
         }
     }
 }
